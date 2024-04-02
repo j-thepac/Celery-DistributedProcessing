a83c3f5f7f0b21552a1aa3c9c0629ee85dae51d9
Deepak <j.thepac@gmail.com>
2024-04-02 08:32:27 +0530

    Update example.py

--- example.py
+++ example.py
@@ -3,8 +3,11 @@
 #Backend = Save Result ( Redis , Mongo ,SQlite ,RabbitMQ ....)
 from celery import Celery
 import time
-app = Celery('tasks', broker='amqps://RabbitMQ_Cloud_@puffin.rmq2.cloudamqp.com/xcxlxzlg',
-             backend='mongodb://localhost:27017/from_celery')
+app = Celery('tasks', broker='redis://localhost:6379/0',backend='redis://localhost:6379/1')
+
+
+# app = Celery('tasks', broker='amqps://RabbitMQ_Cloud_@puffin.rmq2.cloudamqp.com/xcxlxzlg',
+#              backend='mongodb://localhost:27017/from_celery')
 
 @app.task
 def add(x, y):
