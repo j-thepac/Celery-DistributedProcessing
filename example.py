92761101b75d6f5624d831ae25d5f43f99567148
Deepak <j.thepac@gmail.com>
2024-04-02 09:05:06 +0530

    Update example.py

--- example.py
+++ example.py
@@ -5,7 +5,7 @@
 
 from celery import Celery
 import time
-app = Celery('tasks', broker='redis://localhost:6379/0',backend='redis://localhost:6379/1')
+app = Celery('tasks', broker='redis://localhost:6379/0',backend='redis://localhost:6379/0')
 
 # app = Celery('tasks', broker='amqps://RabbitMQ_Cloud_@puffin.rmq2.cloudamqp.com/xcxlxzlg',
 #              backend='mongodb://localhost:27017/from_celery')
